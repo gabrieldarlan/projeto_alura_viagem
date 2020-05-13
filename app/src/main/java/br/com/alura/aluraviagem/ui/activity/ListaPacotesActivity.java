9120735d878b4d340f52f49bcd670f62f95ac36f
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-05-13 20:44:39 -0300

    aula 05 - 01 criando o adapter

--- app/src/main/java/br/com/alura/aluraviagem/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/br/com/alura/aluraviagem/ui/activity/ListaPacotesActivity.java
@@ -3,8 +3,15 @@ package br.com.alura.aluraviagem.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.List;
 
 import br.com.alura.aluraviagem.R;
+import br.com.alura.aluraviagem.dao.PacoteDao;
+import br.com.alura.aluraviagem.model.Pacote;
+import br.com.alura.aluraviagem.ui.adapter.ListaPacotesAdapter;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -12,5 +19,9 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+        List<Pacote> pacotes = new PacoteDao().lista();
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
+
     }
 }
