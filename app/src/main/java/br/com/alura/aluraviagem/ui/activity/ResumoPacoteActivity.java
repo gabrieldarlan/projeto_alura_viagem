3ec3985aaf13c5c7011837f7ac76355c446574f1
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-05-17 20:56:33 -0300

    AULA 02 Adicionando a data e refatorando o layout

--- app/src/main/java/br/com/alura/aluraviagem/ui/activity/ResumoPacoteActivity.java
+++ app/src/main/java/br/com/alura/aluraviagem/ui/activity/ResumoPacoteActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,36 +11,61 @@ import java.math.BigDecimal;
 
 import br.com.alura.aluraviagem.R;
 import br.com.alura.aluraviagem.model.Pacote;
+import br.com.alura.aluraviagem.util.DataUtil;
 import br.com.alura.aluraviagem.util.DiasUtil;
 import br.com.alura.aluraviagem.util.MoedaUtil;
 import br.com.alura.aluraviagem.util.ResourcesUtil;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Resumo do pacote";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
+        setTitle(TITULO_APPBAR);
 
         Pacote pacoteSaoPaulo =
                 new Pacote("São Paulo", "@drawable/sao_paulo_sp",
                         2, new BigDecimal("243.99"));
 
-        TextView local = findViewById(R.id.resumo_pacote_local);
-        local.setText(pacoteSaoPaulo.getLocal());
+        mostraLocal(pacoteSaoPaulo);
+        mostraImagem(pacoteSaoPaulo);
+        mostraDias(pacoteSaoPaulo);
+        mostraPreco(pacoteSaoPaulo);
+        mostraData(pacoteSaoPaulo);
 
+    }
+
+    private void mostraData(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_pacote_data);
+        String dataFormadataDaViagem = DataUtil.periodoEmTexto(pacote.getDias());
+        data.setText(dataFormadataDaViagem);
+    }
+
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
+        String precoFormatado = MoedaUtil.formataParaBrasileiro(pacote.getPreco());
+        preco.setText(precoFormatado);
+    }
+
+    private void mostraDias(Pacote pacote) {
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
+        String diasEmTexto = DiasUtil.formataEmTexto(pacote.getDias());
+        dias.setText(diasEmTexto);
+    }
+
+    private void mostraImagem(Pacote pacote) {
         ImageView imagem = findViewById(R.id.resumo_pacote_imagem);
         Drawable drawableDoPacote =
-                ResourcesUtil.devolveDrawable(this, pacoteSaoPaulo.getImagem());
+                ResourcesUtil.devolveDrawable(this, pacote.getImagem());
         imagem.setImageDrawable(drawableDoPacote);
+    }
 
-        TextView dias = findViewById(R.id.resumo_pacote_dia);
-        String diasEmTexto = DiasUtil.formataEmTexto(pacoteSaoPaulo.getDias());
-        dias.setText(diasEmTexto);
-
-        TextView preco = findViewById(R.id.resumo_pacote_preco);
-        String precoFormatado = MoedaUtil.formataParaBrasileiro(pacoteSaoPaulo.getPreco());
-        preco.setText(precoFormatado);
-
+    private void mostraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_pacote_local);
+        local.setText(pacote.getLocal());
     }
 }
---
+++ app/src/main/java/br/com/alura/aluraviagem/util/DataUtil.java
@@ -0,0 +1,21 @@
+package br.com.alura.aluraviagem.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DataUtil {
+
+    public static final String DIA_E_MES = "dd/MM";
+
+    public static String periodoEmTexto(int dias) {
+        Calendar dataIda = Calendar.getInstance();
+        Calendar dataVolta = Calendar.getInstance();
+        dataVolta.add(Calendar.DATE, dias);
+        SimpleDateFormat formatoBrasileiro = new SimpleDateFormat(DIA_E_MES);
+        String dataIdaFormatada = formatoBrasileiro.format(dataIda.getTime());
+        String dataVoltaFormatada = formatoBrasileiro.format(dataVolta.getTime());
+        return dataIdaFormatada + " - " + dataVoltaFormatada
+                + " de " + dataVolta.get(Calendar.YEAR);
+    }
+
+}
